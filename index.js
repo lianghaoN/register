adf0c27ec85973d8d4e9ce404e780c770bafbb52
lianghaoN <597868880@qq.com>
2020-03-17 06:16:12 +0800

    node1

---
+++ index.js
@@ -0,0 +1,72 @@
+
+$(function(){
+    var $codebtn = $('#codebtn'),
+        $submitbtn = $('#submitbtn');
+
+    $submitbtn.click(function(){
+        user();
+        tell();
+        passwd();
+        code(); 
+        if(!user() || !tell() || !passwd() || !code()) return;
+    })
+
+    $codebtn.click(function(){
+        var t = 5;
+        var time = setInterval(function(){
+            t--;
+            $codebtn.val(t);
+            $codebtn.attr("disabled",true);
+            if(t==0){
+                clearInterval(time);
+                $codebtn.attr("disabled",false);
+                $codebtn.val("获取验证码");
+            }
+        },1000)
+    })
+    function user(){
+        var $data = $('#username'),
+            $msg = $('#usernamemsg');
+        if(!(/^[\da-zA-Z_\u4e00-\u9f5a]{2,10}$/.test($data)) || $data.val() === ''){
+            $msg.html('用户名仅支持中英文，数字和下划线，且不能为纯数字');
+            return false;
+        }else{
+            $msg.html('');
+            return true;
+        }
+    }
+    function tell(){
+        var $data = $('#tellnumber'),
+            $msg = $('#tellnumbermsg');
+        if(!(/^1[3456789]\d{9}$/.test($data)) || $data.val() === ''){
+            $msg.html('手机号码格式不正确');
+            return false;
+        }else{
+            $msg.html('');
+            return true;
+        }
+    }
+    function passwd(){
+        var $data = $('#password'),
+            $msg = $('#passwordmsg');
+        if($data.val() === ''){
+            $msg.html('密码格式不正确');
+            return false;
+        }else{
+            $msg.html('');
+            return true;
+        }
+    }
+    function code(){
+        var $data = $('#codeipt'),
+            $msg = $('#codeiptmsg');
+
+        if($data.val() === ''){
+            $msg.html('请求超时，请稍后再试');
+            return false;
+        }else{
+            $msg.html('');
+            return true;
+        }
+    }
+})
